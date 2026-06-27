b7a4dab095b1a0947db54c4be3f3a8f375e2249b
costavong-pixel <costavong@gmail.com>
2026-06-27 03:13:03 -0400

    Add private config example

---
+++ cloudways/private_html/private_config.example.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'BETA_PASSWORD' => 'change-this-password',
+    'DEEPSEEK_SECRET' => 'paste-secret-here',
+    'DEEPSEEK_MODEL' => 'deepseek-chat',
+    'MAX_INPUT_CHARS' => 4000,
+    'MAX_OUTPUT_TOKENS' => 650,
+];
