b85ac355497ebf3a0afd26d9ce47c97b9405292c
costavong-pixel <costavong@gmail.com>
2026-06-28 12:37:07 -0400

    Trigger Cloudways deploy health check

--- cloudways/public_html/health.php
+++ cloudways/public_html/health.php
@@ -3,5 +3,6 @@ http_response_code(200);
 header('Content-Type: text/plain; charset=utf-8');
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
 header('X-Robots-Tag: noindex, nofollow');
+header('X-Bringora-Health: ok');
 
 echo 'ok';
